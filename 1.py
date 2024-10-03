1bad558386f1f2a98c4b61de9148f7c0dc6445c6
Pelmenmiysnou <pelmenmiysnou@gmail.com>
2024-10-03 23:41:41 +0300

    deleted:    1.py 	deleted:    2.py 	deleted:    3.py

--- 1.py
+++
@@ -1,7 +0,0 @@
-a,b=map(float,input().split())
-if a == 0 or b ==0 :
-    print('bebebe')
-else:
-    print(f'a div b = {a/b}')
-    print(f'b div a = {b/a}')
-
--- 2.py
+++
@@ -1,5 +0,0 @@
-a = float(input())
-if a>20:
-    print(f'итого {a-(a*0.35)} у.е скидка {a*0.35} у.е')
-else:
-    print(a)
