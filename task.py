bb9fe7c27fc7b21c33e9f5cd44513eaf29d1ca51
Pelmenmiysnou <pelmenmiysnou@gmail.com>
2024-10-03 23:43:20 +0300

    new file:   task.py

---
+++ task.py
@@ -0,0 +1,18 @@
+a= int(input())
+s=0
+for i in range(1,a+1):
+    s+=i**3
+print(s)
+
+
+for i in range(1,10):
+    for j in range(1,10):
+        print("%3d" % (i * j), end="")
+    print()
+    
+print() 
+        
+for i in range(9,0,-1):
+    for j in range(1,10):
+        print("%3d" % (i * j), end="")
+    print()
